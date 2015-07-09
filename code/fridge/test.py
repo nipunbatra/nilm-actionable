015b55a7e8ee12940a5de7f8bfff754641c5d3e4
Nipun <nipunb@iiitd.ac.in>
2015-07-09 11:25:38 +0530

    Added pandas test also

--- code/fridge/test.py
+++ code/fridge/test.py
@@ -4,5 +4,8 @@ import os
 print os.getcwd()
 print sys.argv[1], sys.argv[2]
 a, b = sys.argv[1], sys.argv[2]
-with open(a+b+".txt", "w") as f:
-    f.write(a+b)
+
+import pandas as pd
+
+df = pd.HDFStore(a)
+print df.head()
