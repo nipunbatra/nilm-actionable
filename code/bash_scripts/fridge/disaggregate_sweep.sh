4c64ab8bf8703be99759b5fef140d958e082ec7b
Nipun <nipunb@iiitd.ac.in>
2015-07-09 11:22:27 +0530

    Added new figures

--- code/bash_scripts/fridge/disaggregate_sweep.sh
+++ code/bash_scripts/fridge/disaggregate_sweep.sh
@@ -7,27 +7,28 @@ for ((N_STATES=2; N_STATES<5; N_STATES+=1))
     do
     for ((K=2;K<8;K+=1))
         do
+        for((TRAIN=10;TRAIN<100;TRAIN+=10))
+            do
+            OFILE=../../../results/fridge/sweep_results/N${N_STATES}_K${K}.out
+            EFILE=../../../results/fridge/sweep_results/N${N_STATES}_K${K}.err
 
-        OFILE=../../../results/fridge/sweep_results/N${N_STATES}_K${K}.out
-        EFILE=../../../results/fridge/sweep_results/N${N_STATES}_K${K}.err
+            SLURM_SCRIPT=N${N_STATES}_K${K}.pbs
+            CMD='python ../../fridge/test.py ~/wikienergy-2.h5 '$N_STATES' '$K' '$TRAIN''
+            echo $CMD
 
-        SLURM_SCRIPT=N${N_STATES}_K${K}.pbs
-        CMD='python ../../fridge/test.py '$N_STATES' '$K''
-        echo $CMD
+            #rm ${SLURM_SCRIPT}
+            echo "#!/bin/sh" > ${SLURM_SCRIPT}
+            #echo $pwd > ${SLURM_SCRIPT}
+            echo '#SBATCH --time=02:0:00' >> ${SLURM_SCRIPT}
+            echo '#SBATCH --mem=16' >> ${SLURM_SCRIPT}
+            echo '#SBATCH -o "./'${OFILE}'"' >> ${SLURM_SCRIPT}
+            echo '#SBATCH -e "./'${EFILE}'"' >> ${SLURM_SCRIPT}
+            #echo 'cd $SLURM_SUBMIT_DIR' >> ${SLURM_SCRIPT}
+            echo ${CMD} >> ${SLURM_SCRIPT}
 
-        #rm ${SLURM_SCRIPT}
-        echo "#!/bin/sh" > ${SLURM_SCRIPT}
-        #echo $pwd > ${SLURM_SCRIPT}
-        echo '#SBATCH --time=4-00:0:00' >> ${SLURM_SCRIPT}
-        echo '#SBATCH --mem=4' >> ${SLURM_SCRIPT}
-        echo '#SBATCH -o "./'${OFILE}'"' >> ${SLURM_SCRIPT}
-        echo '#SBATCH -e "./'${EFILE}'"' >> ${SLURM_SCRIPT}
-        #echo 'cd $SLURM_SUBMIT_DIR' >> ${SLURM_SCRIPT}
-        echo ${CMD} >> ${SLURM_SCRIPT}
-
-        #cat ${SLURM_SCRIPT}
-        sbatch ${SLURM_SCRIPT}
-
+            #cat ${SLURM_SCRIPT}
+            sbatch ${SLURM_SCRIPT}
+            done
         done
     done
 
