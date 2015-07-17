4b424bc7583995dfdfb6d0336858f2e2815cd12a
Nipun <nipunb@iiitd.ac.in>
2015-07-17 18:06:05 +0530

    removed group

--- code/bash_scripts/fridge/disaggregate_fridge_breakdown.sh
+++ code/bash_scripts/fridge/disaggregate_fridge_breakdown.sh
@@ -12,29 +12,26 @@ for ((N_STATES=2; N_STATES<5; N_STATES+=1))
         do
         for((ii=0;ii<$num_algos;ii+=1))
             do
-            for((group=0;group<31;group+=1))
-                do
-                algo=${algos[$ii]}
-                OFILE=../../../results/fridge/sweep_results/N${N_STATES}_K${K}_T${TRAIN}_"$algo"_G${group}.out
-                EFILE=../../../results/fridge/sweep_results/N${N_STATES}_K${K}_T${TRAIN}_"$algo"_G${group}.err
+            algo=${algos[$ii]}
+            OFILE=../../../results/fridge/sweep_results/N${N_STATES}_K${K}_T${TRAIN}_"$algo"_G${group}.out
+            EFILE=../../../results/fridge/sweep_results/N${N_STATES}_K${K}_T${TRAIN}_"$algo"_G${group}.err
 
-                SLURM_SCRIPT=N${N_STATES}_K${K}_T${TRAIN}_"$algo"_G${group}.pbs
-                CMD='python ../../fridge/compressor_durations_optimised_parallel '$N_STATES' '$K' '$algo''
-                echo $CMD
+            SLURM_SCRIPT=N${N_STATES}_K${K}_T${TRAIN}_"$algo".pbs
+            CMD='python ../../fridge/compressor_durations_optimised_parallel '$N_STATES' '$K' '$algo''
+            echo $CMD
 
-                #rm ${SLURM_SCRIPT}
-                echo "#!/bin/sh" > ${SLURM_SCRIPT}
-                #echo $pwd > ${SLURM_SCRIPT}
-                echo '#SBATCH --time=1-02:0:00' >> ${SLURM_SCRIPT}
-                echo '#SBATCH --mem=16' >> ${SLURM_SCRIPT}
-                echo '#SBATCH -o "./'${OFILE}'"' >> ${SLURM_SCRIPT}
-                echo '#SBATCH -e "./'${EFILE}'"' >> ${SLURM_SCRIPT}
-                #echo 'cd $SLURM_SUBMIT_DIR' >> ${SLURM_SCRIPT}
-                echo ${CMD} >> ${SLURM_SCRIPT}
+            #rm ${SLURM_SCRIPT}
+            echo "#!/bin/sh" > ${SLURM_SCRIPT}
+            #echo $pwd > ${SLURM_SCRIPT}
+            echo '#SBATCH --time=1-02:0:00' >> ${SLURM_SCRIPT}
+            echo '#SBATCH --mem=16' >> ${SLURM_SCRIPT}
+            echo '#SBATCH -o "./'${OFILE}'"' >> ${SLURM_SCRIPT}
+            echo '#SBATCH -e "./'${EFILE}'"' >> ${SLURM_SCRIPT}
+            #echo 'cd $SLURM_SUBMIT_DIR' >> ${SLURM_SCRIPT}
+            echo ${CMD} >> ${SLURM_SCRIPT}
 
-                #cat ${SLURM_SCRIPT}
-                sbatch ${SLURM_SCRIPT}
-                done
+            #cat ${SLURM_SCRIPT}
+            sbatch ${SLURM_SCRIPT}
             done
         done
     done
